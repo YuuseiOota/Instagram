9bc88b91f0cfcec6324ceb954fbb4fe62f448531
YuuseiOota <porte0625@gmail.com>
2018-07-08 21:31:24 +0900

    レッスン通りに実装した

---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by UxU on 2018/07/07.
+//  Copyright © 2018年 UxU. All rights reserved.
+//
+
+import Foundation
+
+struct Const {
+    static let PostPath = "posts"
+}
